a90be2804bf8007e55ef6ea0ddab01ae4f72f50b
batushka11 <119219538+batushka11@users.noreply.github.com>
2023-10-29 15:43:52 +0100

    Update mx_find_index.c

--- libmx/src/mx_find_index.c
+++ libmx/src/mx_find_index.c
@@ -2,7 +2,7 @@
 
 int mx_find_index(char *arr[], int size, const char *c) {
     for (int i = 0; i < size; i++) {
-        if (arr[i] != NULL && strcmp(arr[i], c) == 0) {
+        if (arr[i] != NULL && mx_strcmp(arr[i], c) == 0) {
             return i;
         }
     }
